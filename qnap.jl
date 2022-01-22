512bf122bc61ce971dea27c71538efa34e73b25a
Glen Hertz <glen.hertz@gmail.com>
2022-01-22 16:58:33 -0500

    Also move off of incoming drive

--- qnap.jl
+++ qnap.jl
@@ -7,3 +7,7 @@ dst_root="/mnt/zpool/tank/media/Pictures"
 src_dirs = [ "/mnt/zpool/tank/syncthing/Pixel4a" ]
 organize_photos(src_dirs, dst_root; rm_src, dry_run, photo_suffix="_Glen")
 
+# Move photos off of IncomingPhotos drive to the photo library
+rm_src = true
+organize_photos(["/mnt/zpool/tank/IncomingPhotos"], dst_root; rm_src, dry_run)
+
